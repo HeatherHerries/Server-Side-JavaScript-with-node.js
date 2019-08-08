1ddb6caef504be172c6489f0f333ff7775d81015
Heather Herries <heatherherries@Heathers-MacBook-Air.local>
2019-08-07 19:10:57 -0600

    Assignment Basic Nope App: complete

---
+++ node-greeting-app/goodbye.js
@@ -0,0 +1,15 @@
+exports.goodbyeEnglish = function() {
+    console.log("Goodbye");
+}
+
+exports.goodbyeJapanese = function() {
+    console.log("Sayōnara");
+}
+
+exports.goodbyeSpanish = function() {
+    console.log("Adiòs");
+}
+
+exports.goodbyeIcelandic = function() {
+    console.log("Bless")
+}
---
+++ node-greeting-app/main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const goodbye = require("./goodbye.js");
+
+//"Hello"
+greetings.greetingEnglish();
+
+//"Konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Halló"
+greetings.greetingIcelandic();
+
+//"Goodbye"
+goodbye.goodbyeEnglish();
+
+//"Sayōnara"
+goodbye.goodbyeJapanese();
+
+//"Adiòs"
+goodbye.goodbyeSpanish();
+
+//"Bless"
+goodbye.goodbyeIcelandic();
